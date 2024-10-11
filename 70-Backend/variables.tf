e2e299e070cce9c49c3b1bbb1064b14f7cf83d18
venugopalsantosh <venugopalsantosh.p@hotmail.com>
2024-10-12 00:17:04 +0530

    terraform

---
+++ 70-Backend/variables.tf
@@ -0,0 +1,24 @@
+variable "common_tags"{
+    default = {
+        Project = "Expense"
+        Environemnt = "Dev"
+        Terraform = true
+
+    }
+}
+
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "component"{
+    default = "backend"
+}
+
+
+
+
